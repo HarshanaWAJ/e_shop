644f09b395882c3d9fdd3318d53b961beb5c4f4b
HarshanaWAJ <janithharshana2019@gmail.com>
2024-03-10 23:02:16 +0530

    email login backend

--- lib/screens/login._screen.dart
+++ lib/screens/login._screen.dart
@@ -3,7 +3,6 @@ import 'package:e_shop/components/squre_tile.dart';
 import 'package:e_shop/components/text_field.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
 class LoginScreen extends StatelessWidget {
   LoginScreen({super.key});
